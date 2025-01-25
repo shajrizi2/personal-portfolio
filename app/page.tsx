2afaeb77146908695a4cd6c19de5ca235753ab91
Shkelqim <shajrizi2@gmail.com>
2025-01-26 00:19:47 +0100

    added styles and hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px10">
       <div className="max-w-7xl w-full">
-        <h1>Shkelqim's Portfolio</h1>
-        <p>This is my professional portfolio where I showcase my work.</p>
+        <Hero />
       </div>
     </main>
   );
