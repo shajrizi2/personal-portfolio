ce612ba6e48bca405246747ceb400d168109807f
Shkelqim <shajrizi2@gmail.com>
2025-02-17 00:26:44 +0100

    added new spotlight component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import { FaLocationArrow } from "react-icons/fa6";
 
 import { Spotlight } from "./ui/Spotlight";
+import { SecondSpotlight } from "./ui/SecondSpotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import PrimeButton from "./ui/PrimeButton";
 import { SlActionRedo } from "react-icons/sl";
@@ -13,15 +14,18 @@ const Hero = () => {
        *  Link: https://ui.aceternity.com/components/spotlight
        */}
       <div>
-        <Spotlight
+        <SecondSpotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
         />
-        <Spotlight
+        <SecondSpotlight
           className="h-[80vh] w-[50vw] top-10 left-full"
           fill="purple"
         />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+        <SecondSpotlight
+          className="left-80 top-28 h-[80vh] w-[50vw]"
+          fill="blue"
+        />
       </div>
 
       {/**
@@ -44,7 +48,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Dynamic Web Magic with Next.js—Built for Performance & User Delight.
           </p>
 
           {/**
@@ -53,7 +57,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="From Concept to Code: Crafting Efficient and Scalable Digital Products."
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
